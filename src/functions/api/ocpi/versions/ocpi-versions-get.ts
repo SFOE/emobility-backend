964691d07fcb00e4f75d48007184900b1a9d2b2c
Marc <marc.schnydrig@ti8m.ch>
2026-05-15 15:26:42 +0200

    refactor: split version and version details handler into separate files (#28)

---
+++ src/functions/api/ocpi/versions/ocpi-versions-get.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { getOCPIVersions } from '/opt/nodejs/db/ocpi-version/ocpi-version.db';
+import { ErrorHandler } from '/opt/nodejs/api/error/api-error-handler';
+import { prepareOCPIResponse } from '/opt/nodejs/utils/api.utils';
+
+/**
+ * Handles GET /ocpi/versions.
+ * Returns all OCPI versions supported by the emobility system,
+ * including the URLs to their corresponding version detail endpoints.
+ */
+export const handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('GET versions:', JSON.stringify(event, null, 2));
+
+  try {
+    const versions = await getOCPIVersions();
+    return prepareOCPIResponse(versions);
+  } catch (error) {
+    return ErrorHandler.handleError(error);
+  }
+};
+
